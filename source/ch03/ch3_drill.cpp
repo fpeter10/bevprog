ded7115c62252bb41eeea465cf4e23707691c7a3
Fauszt Péter <fauszt.peter92@gmail.com>
2021-09-27 20:04:07 +0200

    upload

--- source/ch03/ch3_drill.cpp
+++ source/ch03/ch3_drill.cpp
@@ -5,7 +5,9 @@ int main()
 	string first_name, friend_name;
 	char friend_sex= '0';
 	int age;
-	cout<< "Please enter the name of the person you want to write to: "<< endl;
+	
+	//questions
+	cout<< "Please enter the name of the person you want to write to: ";
 	cin>> first_name;
 	cout << "Please enter a friend name! ";
 	cin >> friend_name;
@@ -14,22 +16,22 @@ int main()
 	cout<< "Plase enter the age of your friend ";
 	cin >> age;
 	
+	// letter
 	cout<< endl<< "Dear " << first_name <<","<<endl;
 	cout << "How are you, I hope everything is ok with you in this pandemic time. I really hope that we can meet in person soon. "<< endl;
-
-
 	cout<< "Have you seen " << friend_name << " lately? "<< endl;
+	
 	if (friend_sex == 'm'){
 			cout<< "If you see "<< friend_name << " please ask him to call me!"<< endl;
 		}
 	else if (friend_sex == 'f'){
 			cout<< "If you see "<< friend_name << " please ask her to call me!" << endl;
 		}
-
 	
-		if (age <=0 || age >110){
+	if (age <=0 || age >110){
 		simple_error("You are kidding! ");	
 	}	
+	
 	cout << "I hear you just had a birthday, and you are " << age << " years old :)" <<" Happy birthday."<< endl;
 	if (age < 12){
 		cout<<"Next year you will be " << age+1 <<"." <<endl;
@@ -43,5 +45,4 @@ int main()
 	cout << "Yours sincerelly, "<< endl << endl<< endl;
 	cout << "Péter"<< endl;
 	return 0;
-
 }
